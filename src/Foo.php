538cd87b12f2c20e62378e6e93116a979a3986bf
Caio Lucena <caioflucena@gmail.com>
2022-05-26 09:35:40 -0300

    initial

---
+++ src/Foo.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Src;
+
+class Foo
+{
+    private $foo;
+    private $bar;
+
+    public function __construct($foo, $bar)
+    {
+        $this->foo = $foo;
+        $this->bar = $bar;
+    }
+
+    public function getFoo()
+    {
+        return $this->foo;
+    }
+
+    public function getBar()
+    {
+        return $this->bar;
+    }
+}
---
+++ tests/unit/FooTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use ReflectionObject;
+use Src\Foo;
+
+/**
+ * 
+ */
+class FooTest extends TestCase
+{
+    public function test_constructor()
+    {
+        $foo = new Foo("foo", "bar");
+
+        $reflectionInstance = new ReflectionObject($foo);
+        $reflectionFoo = $reflectionInstance->getProperty("foo");
+        $reflectionFoo->setAccessible(true);
+        $this->assertEquals("foo", $reflectionFoo->getValue($foo));
+
+        $reflectionInstance = new ReflectionObject($foo);
+        $reflectionBar = $reflectionInstance->getProperty("bar");
+        $reflectionBar->setAccessible(true);
+        $this->assertEquals("bar", $reflectionBar->getValue($foo));
+    }
+
+    public function test_getFoo()
+    {
+        $foo = new Foo("foo", "bar");
+        $this->assertEquals("foo", $foo->getFoo());
+    }
+
+    public function test_getBar()
+    {
+        $foo = new Foo("foo", "bar");
+        $this->assertEquals("bar", $foo->getBar());
+    }
+}
